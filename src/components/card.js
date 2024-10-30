9f81cab601594ef82def349ee0939eb6d53f682c
Tomi Jokinen <tomi.w.jokinen@gmail.com>
2024-10-29 21:15:33 -0600

    fix project icon and add padding between projects

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ export default function Card({ organization, iconClassName, title, date, descrip
   const newDesc = description.split('\n').map((str, i) => <p key={i} className="text-base col-span-8">{str}</p>);
 
   return (
-    <div className="grid grid-cols-8 text-4xl">
+    <div className="grid grid-cols-8 text-4xl pb-6">
       <p className="text-base col-span-8">{organization}</p>
       {!disableIcon ? <i className={iconClassName}></i> : <></>}
       <p className="text-xl col-span-7">{title}</p>
